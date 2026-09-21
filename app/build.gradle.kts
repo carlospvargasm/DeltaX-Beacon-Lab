53f8dabe9f67bd445ba5675103631178e33919bd
carlospvargasm <carlos.p.vargas.m@gmail.com>
2026-09-21 17:11:03 -0300

    Set Estimote diagnostic package and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,6 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
-
-android {
-    namespace = "com.deltax.beaconlab"
-    compileSdk = 35
-    defaultConfig {
-        applicationId = "com.deltax.quickshare.cvm20260920"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 4
-        versionName = "1.0-quickshare"
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+android { namespace="com.deltax.beaconlab"; compileSdk=35
+ defaultConfig { applicationId="com.deltax.estimotelab.cvm20260921"; minSdk=26; targetSdk=35; versionCode=5; versionName="2.0-estimote-diagnostic" }
+ compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
 }
 kotlin { jvmToolchain(17) }
